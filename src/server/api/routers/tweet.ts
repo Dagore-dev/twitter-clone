710000e28794fa7a6bdaf9bba06e26cb0d8a626c
Dagore <david.gomez.redondo.dev@gmail.com>
2023-05-14 09:20:48 +0200

    infinite tweets procedures and schema updated

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
+  publicProcedure,
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
@@ -10,5 +11,58 @@ export const tweetRouter = createTRPCRouter({
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
       return await ctx.prisma.tweet.create({ data: { content, userId: ctx.session.user.id } })
+    }),
+  infiniteFeed: publicProcedure
+    .input(z.object({ 
+      limit: z.number().optional(),
+      cursor: z.object({ id: z.string(), createdAt: z.date() }).optional() 
+    }))
+    .query(async ({ input: { limit = 10, cursor }, ctx }) => {
+      const currentUserId = ctx.session?.user.id
+
+      const tweets = await ctx.prisma.tweet.findMany({
+        take: limit + 1,
+        cursor: cursor ? { createdAt_id: cursor } : undefined,
+        orderBy: [{ createdAt: 'desc' }, { id: 'desc' }],
+        select: {
+          id: true,
+          content: true,
+          createdAt: true,
+          _count: { select: { likes: true } },
+          likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+          user: {
+            select: {
+              id: true,
+              name: true,
+              image: true
+            }
+          }
+        }
+      })
+
+      let nextCursor: typeof cursor | undefined
+      if (tweets.length > limit) {
+        const nextTweet = tweets.pop()
+        if (nextTweet != null) {
+          nextCursor = {
+            id: nextTweet.id,
+            createdAt: nextTweet.createdAt
+          }
+        }
+      }
+
+      return {  
+        nextCursor,
+        tweets: tweets.map(tweet => {
+          return {
+            id: tweet.id,
+            content: tweet.content,
+            createdAt: tweet.createdAt,
+            likeCount: tweet._count.likes,
+            user: tweet.user,
+            likedByBe: tweet.likes?.length > 0
+          }
+        })
+      }
     })
 });
