0ac4a501dffa96277d431c1f3c41fbfe876c812b
Dagore <david.gomez.redondo.dev@gmail.com>
2023-05-14 18:49:56 +0200

    loading spinner

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,6 +1,7 @@
 import InfiniteScroll from "react-infinite-scroll-component"
 import type Tweet from "~/interfaces/Tweet"
 import { TweetCard } from "./TweetCard"
+import { LoadingSpinner } from "./LoadingSpinner"
 
 interface Props {
   tweets?: Tweet[]
@@ -11,7 +12,7 @@ interface Props {
 }
 
 export function InfiniteTweetList ({ tweets, isError, isLoading, hasMore, fetchNewTweets }: Props) {
-  if (isLoading) return <h1>Loading...</h1>
+  if (isLoading) return <LoadingSpinner />
   if(isError) return <h1>Error</h1>
   if (tweets == null || tweets.length === 0) {
     return <h2 className="my-4 text-center text-2xl text-gray-500">No tweets</h2>
@@ -22,7 +23,7 @@ export function InfiniteTweetList ({ tweets, isError, isLoading, hasMore, fetchN
       dataLength={tweets.length}
       next={fetchNewTweets}
       hasMore={hasMore}
-      loader="Loading..."
+      loader={<LoadingSpinner />}
     >
       {
         tweets.map(tweet => <TweetCard key={tweet.id} {...tweet} />)
---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import { VscRefresh } from 'react-icons/vsc'
+
+export function LoadingSpinner ({ big = false }) {
+  const sizeClasses = big ? 'w-16 h-16' : 'w-10 h-10'
+
+  return <div className='flex justify-center p-2'>
+    <VscRefresh className={`animate-spin ${sizeClasses}`} />
+  </div>
+}
