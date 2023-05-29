4e351b35b9eedab589639848c7cd997e4fe73af9
Dagore <david.gomez.redondo.dev@gmail.com>
2023-05-29 22:05:05 +0200

    protect edit profile endpoint

--- src/pages/profiles/edit/[id].tsx
+++ src/pages/profiles/edit/[id].tsx
@@ -10,15 +10,21 @@ import { api } from "~/utils/api";
 import ErrorPage from "next/error";
 import { DetailHeader } from "~/components/DetailHeader";
 import { EditProfileForm } from "~/components/EditProfileForm";
+import { useSession } from "next-auth/react";
 
 const EditProfilePage: NextPage<
   InferGetStaticPropsType<typeof getStaticProps>
 > = ({ id }) => {
   // No loading state since we made this query on getStaticProps
   const { data: profile } = api.profile.getById.useQuery({ id });
+  const session = useSession()
 
   if (profile == null || profile.name == null)
     return <ErrorPage statusCode={404} />;
+
+  if (session.status !== 'authenticated' || session.data.user.id !== id) {
+    return <ErrorPage statusCode={401} />
+  }
 
   return (
     <>
