05d4c054ebe9b573acaa6c5fac99d86bfc4ecb11
Dagore <david.gomez.redondo.dev@gmail.com>
2023-06-08 15:51:32 +0200

    refactor extract services from tweet router

---
+++ src/server/api/services/tweet/infiniteProfileFeed.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { publicProcedure } from "../../trpc";
+import getInfinityTweets from "./getInfinityTweets";
+
+export const infiniteProfileFeed = publicProcedure
+.input(
+  z.object({
+    userId: z.string(),
+    limit: z.number().optional(),
+    cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+  })
+)
+.query(async ({ input: { userId, limit = 10, cursor }, ctx }) => {
+  return await getInfinityTweets({
+    limit,
+    cursor,
+    ctx,
+    whereClause: { userId },
+  });
+})
